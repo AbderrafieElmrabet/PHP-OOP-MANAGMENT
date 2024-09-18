2db0e76e93bd1b69bd587d976949838ed764bad9
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2024-09-18 10:38:19 +0100

    commit

---
+++ tutorials/tuto-3.php
@@ -0,0 +1,15 @@
+<?php
+class Animal {
+    public $nom;
+    public $age;
+
+    public function manger() {
+        echo "Je mange.";
+    }
+}
+
+class Chat extends Animal {
+    public function miauler() {
+        echo "Miaou !";
+    }
+}
